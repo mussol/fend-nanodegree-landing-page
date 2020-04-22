2c560b69ea5e99a716aef509b9dc4373cad4b9a1
mussol <gemma.valls@gmail.com>
2020-04-22 22:26:45 +0200

    feat highlight active section

--- js/app.js
+++ js/app.js
@@ -26,6 +26,17 @@ const sections = document.querySelectorAll("section");
  * Start Helper Functions
  * 
 */
+function isInViewport (el) {
+    let rect = el.getBoundingClientRect();
+    return (
+        rect.top > 0 &&
+        rect.left > 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+// help for function above: https://knowledge.udacity.com/questions/124306 and https://tinyurl.com/yd3b3oy6
+
 
 
 
@@ -41,6 +52,7 @@ document.addEventListener('DOMContentLoaded', buildNavMenu); //trying to tackle 
 
 // Add class 'active' to section when near top of viewport
 
+document.addEventListener("scroll", activateSection);
 
 // Scroll to anchor ID using scrollTO event
 
@@ -71,4 +83,13 @@ function buildNavMenu () {
 
 // Set sections as active
 
+function activateSection () {
+    for (let i = 0; i < sections.length; i++) {
+        if (isInViewport(sections[i])) {
+            sections[i].classList.add("your-active-class");
+        } else {
+            sections[i].classList.remove("your-active-class");
+        }
+    }
+};
 
